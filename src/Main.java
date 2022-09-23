86955ae598782ae259e317974f2cde74457aafec
Vladislav Sizov <vladislav.sizov@rambler.ru>
2022-09-23 20:58:31 +0300

    DevideByZero

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,20 @@ public class Main {
         Calculator calc = Calculator.instance.get();
 
         int a = calc.plus.apply(3, 2);
-        int b = calc.minus.apply(4,1);
-        int c = calc.multiply.apply(5,6);
-        int d = calc.devide.apply(6, 2);
+        int b = calc.minus.apply(4, 1);
+        int c = calc.multiply.apply(5, 6);
+
+        try {
+            int d = calc.devide.apply(6, 0);
+            calc.println.accept(d);
+        } catch (ArithmeticException exc) {
+            System.out.println("Попытка деления на ноль");
+        }
 
         calc.println.accept(a);
         calc.println.accept(b);
         calc.println.accept(c);
-        calc.println.accept(d);
+
 
         int e = calc.pow.apply(3);
         calc.println.accept(e);
@@ -21,7 +27,7 @@ public class Main {
 
         boolean g = calc.isPositive.apply(-5);
         calc.printlnb.accept(g);    //Не работало тут, так как передавался тип переменной Integer, а результатом g является boolean
-                                    //Поэтому создал еще одну реализацию println для boolean
+        //Поэтому создал еще одну реализацию println для boolean
 
     }
 }
